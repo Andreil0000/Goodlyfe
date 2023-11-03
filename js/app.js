d21b4cc9369110e923b162bb13cfb79eb1506f9a
Andreil0000 <126258689+Andreil0000@users.noreply.github.com>
2023-11-03 14:18:38 +0500

    привет андрей

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Menu
+
+function openModal() {
+   document.getElementById("modal").style.top = "-20px";
+}
+
+function closeModal() {
+   document.getElementById("modal").style.top = "-450px";
+}
